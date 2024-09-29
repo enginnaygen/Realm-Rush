ce12d7aa718195bdc53ca59891211427bbf3f1c9
Engin <enginaygen77@gmail.com>
2024-09-29 18:11:55 +0300

    Damaging Enemies

---
+++ Realm Rush/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] int maxHitPoints = 5;
+    [SerializeField] int currentHitPoint = 0;
+
+    private void OnParticleCollision(GameObject other)
+    {
+        currentHitPoint++;
+
+        if(currentHitPoint >= maxHitPoints)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
